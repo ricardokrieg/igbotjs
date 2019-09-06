79cf7a50a18b066c0259e71ff1aee3b6babf8f1c
Ricardo Franco <ricardo.krieg@gmail.com>
2019-09-06 20:57:47 -0300

    sendDM

---
+++ src/sendDM.js
@@ -0,0 +1,33 @@
+const inquirer = require('inquirer');
+
+const Bot = require('./bot');
+const { logger, sleep } = require('./utils');
+
+const log = (message) => logger('SendDM', message);
+
+
+(async () => {
+  log('Start');
+
+  const { username } = (await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Username',
+    },
+  ]));
+
+  while(true) {
+    try {
+      await (new Bot({ username })).sendDM({ target: 'ricardokrieg' });
+      break;
+    } catch (e) {
+      console.log(e);
+    }
+
+    console.log('Try again...');
+    await sleep(20000);
+  }
+
+  log('End');
+})();
