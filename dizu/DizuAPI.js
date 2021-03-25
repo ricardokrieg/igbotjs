391e410bd2b6f53432cd3ca9139673bb68f4e1ec
Emiliano Qaqi <emilianoqaqi@msn.com>
2021-03-25 10:19:30 -0300

    updating dizu cookies

--- dizu/DizuAPI.js
+++ dizu/DizuAPI.js
@@ -33,7 +33,8 @@ class DizuAPI {
     this.cookieJar = jar(this.cookieStore);
 
     // const cookies = `__cf_bm=218e455aaf6728028bce3a4d51c5b475b5913f96-1615464177-1800-AVcC+EwsmecInI/6RdVm7KX3RIGrSyrvjNrP8oMcvKaITZ6rKM6hosSZHi/ZkZmluJAhzPGqC4ArGsW7dqqePqlYHlrbxaAx9gkL78tqyAO/LcbEfzLAGgIOxmRCgovQIQ==; _ga=GA1.3.523335642.1615406726; _gat_gtag_UA_160075623_1=1; _gid=GA1.3.139030887.1615406726; crsftoken=a9b41123218245c10ee9ce888c56d533:e6764317a8c7f8490bf1e51fba2a10b6; __cfduid=db4c03bac499cfcfb473de40b502238f61615030609`;
-    const cookies = `_ga=GA1.3.523335642.1615406726; _gat_gtag_UA_160075623_1=1; _gid=GA1.3.19306637.1615776415; __cf_bm=0e60de321ede0a44e80949195917ca202d1093e4-1616676732-1800-AbWLlaSpLYy4NXUTwRNC0Amc3XsoBhUIlk+xyS6/vSiXI86lCdBhFS6j2y2RWXlzU5QEzRFYeR8X7dfABsdzbsHT8zx9lgsqURo4r7lUWTAt24IeTW+0MlbwsSwpwBeK2g==; crsftoken=e018c250f0b1ab32649a4e7d08fc0909:e4e946164ba6d6633da07549490e966f; __cfduid=db4c03bac499cfcfb473de40b502238f61615030609`;
+    // const cookies = `_ga=GA1.3.523335642.1615406726; _gat_gtag_UA_160075623_1=1; _gid=GA1.3.19306637.1615776415; __cf_bm=0e60de321ede0a44e80949195917ca202d1093e4-1616676732-1800-AbWLlaSpLYy4NXUTwRNC0Amc3XsoBhUIlk+xyS6/vSiXI86lCdBhFS6j2y2RWXlzU5QEzRFYeR8X7dfABsdzbsHT8zx9lgsqURo4r7lUWTAt24IeTW+0MlbwsSwpwBeK2g==; crsftoken=e018c250f0b1ab32649a4e7d08fc0909:e4e946164ba6d6633da07549490e966f; __cfduid=db4c03bac499cfcfb473de40b502238f61615030609`;
+	  const cookies = `__cfduid=d67c87f22941c4798da92da82a41ba6b71616677462; __cf_bm=bcef4b9acb7691c1aec0e02d36fb3b3144a7f05b-1616677464-1800-Aa0/X3xnOQum2EfA/K0VtCxTnQg5+dhQbx6bGFIcHOfQmvjeFgHJvfvuW9UKyzGmvF2stp03uPM6/9ozWSNCBNPLyvM7AxurLvK2CR1wh/BJg/ToXGdYPjksCooSwcjuiQ==; _ga=GA1.3.974360772.1616677466; _gid=GA1.3.308710921.1616677466; crsftoken=144c365f857d6269ff46d1bb11839927:a4035c207b3a75191370ff3db541487b; _gat_gtag_UA_160075623_1=1`;
     for (let cookie of cookies.split(`;`)) {
       this.cookieJar.setCookie(cookie, `https://dizu.com.br/`);
     }
@@ -98,7 +99,7 @@ class DizuAPI {
   }
 
   getTaskUrl(accountId) {
-    return `/painel/listar_pedido/?&conta_id=${accountId}&twitter_id=Twitter&tiktok_id=TikTok&tarefa10=0&curtida05=0&acoesmg=0&estado=FL`;
+    return `/painel/listar_pedido/?&conta_id=${accountId}&twitter_id=Twitter&tiktok_id=TikTok&tarefa10=0&curtida05=0&acoesmg=0`;
   }
 }
 
